a493eff4b6c5903d11b4ffaaf4c18b42db0a654f
PITO <pitoab@wp.pl>
2021-07-27 10:50:56 +0200

    Wystawia trójki liczb bez zera

--- trojki_suma_liczba_pierwsza/trojki_suma_liczba_pierwsza.cpp
+++ trojki_suma_liczba_pierwsza/trojki_suma_liczba_pierwsza.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 /////deklaracje zmiennych i stałych globalnych
-#define number 5	//ile liczb ma sprawdzić
-int tab[500][3];
+#define number 20	//ile liczb ma sprawdzić
+int tab[1000][3];
 
 /////deklaracje funkcji
 void display_arrow(int t[][3], int s);
 void three_numbers(int& size, int tab[][3]);
-bool check_to_nuber_exist(int i, int j, int k, int s);
+bool check_to_nuber_exist(int a, int b, int c, int s);
 int prime_number(int how_meny);
 void save_prime_number(int number_of_prime_numbers, int* all_prime_numb);
 bool it_is_prime_number(int l);
@@ -53,9 +53,9 @@ void three_numbers(int &size, int tab[][3]) {
 	}
 }
 //sprawdza czy nr jest w tablicy
-bool check_to_nuber_exist(int i,int j, int k, int s) {
+bool check_to_nuber_exist(int a,int b, int c, int s) {
 	for (int i = 0; i < s+1; i++) {
-		if (l == tab[i][0]) {
+		if (a == tab[i][0] || b == tab[i][1] || c == tab[i][2]) {
 			return false;
 		}	
 	}
